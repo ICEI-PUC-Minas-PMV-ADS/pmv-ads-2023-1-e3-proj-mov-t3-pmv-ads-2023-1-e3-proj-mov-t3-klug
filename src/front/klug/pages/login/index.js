2fb30a970a0ed567356674b51fc0452b96443d25
Mikaelly Carneiro <mikaellyscarneiro@gmail.com>
2023-04-19 16:32:31 -0300

    Implementa uma simulação da tela de login para a tela principal

--- src/front/klug/pages/login/index.js
+++ src/front/klug/pages/login/index.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import styles from "./styles";
 
-export default function Login() {
+export default function Login({handleLogin}) {
   return (
     <View style={styles.container}>
       <Text>Página Login!</Text>
+      <Button title="Simular Login" onPress={()=> handleLogin(true)} />
       <StatusBar style="auto" />
     </View>
   );
