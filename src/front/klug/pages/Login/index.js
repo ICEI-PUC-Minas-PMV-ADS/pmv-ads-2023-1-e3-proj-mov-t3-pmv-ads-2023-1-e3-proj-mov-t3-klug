c27572444b8332bf21f2055ecded252272686895
Bruno Ruas <bruno.ruas2@gmail.com>
2023-04-26 20:36:20 -0400

    login page

--- src/front/klug/pages/Login/index.js
+++ src/front/klug/pages/Login/index.js
@@ -1,15 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
 import styles from "./styles";
+import { StatusBar } from 'expo-status-bar';
 
-export default function Login({handleLogin}) {
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import { TextInput, Text, Button } from 'react-native-paper';
+
+export default function Login({ handleLogin }) {
   return (
     <View style={styles.container}>
-      <Text>Página Login!</Text>
-      <Button title="Simular Login" onPress={()=> handleLogin(true)} />
-      <StatusBar style="auto" />
+        <View style={styles.header}>
+          <Text style={styles.title}>Login</Text>
+        </View>
+
+        <Image
+          source={{ uri: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e3-proj-mov-t3-pmv-ads-2023-1-e3-proj-mov-t3-klug/main/docs/img/logo-klug.png' }}
+          style={styles.Logo} />
+
+        <View style={styles.form}>
+          <Text>E-mail</Text>
+          <TextInput
+            label="Digite seu e-mail"
+            mode="outlined"
+            borderRadius={10}
+            borderWidth={0}
+            style={styles.input} />
+
+          <Text>Senha</Text>
+          <TextInput label="Digite sua senha" mode="outlined" style={styles.input} />
+
+          <Text style={styles.lostpsw}>Esqueci a Senha</Text>
+        </View>
+
+
+        <Button mode="contained" onPress={() => handleLogin(true)} style={styles.button}>
+          Entrar
+        </Button>
+
+        <Text style={{ alignSelf: 'center' }}>Criar Conta</Text>
+
     </View>
   );
 }
+
 //TODO: Bruno alterar aqui.
--- src/front/klug/pages/Login/styles.js
+++ src/front/klug/pages/Login/styles.js
@@ -7,6 +7,44 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    header: {
+        marginTop: 60,
+        marginBottom: 30,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    Logo: {
+        width: 200,
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    form: {
+        paddingHorizontal: 10
+    },
+    input: {
+        marginBottom: 20,
+        width: '100%',
+        borderRadius: 10,
+        borderWidth: 0,
+    },
+    lostpsw: {
+        marginBottom: 20,
+        alignSelf: 'center',
+        textAlign: 'right'
+    },
+    button: {
+        width: '150%',
+        marginBottom: 10,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+    },
 });
 
 export default styles;
