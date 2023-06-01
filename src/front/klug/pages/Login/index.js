6b0adc8428356b982c15273de7a0cbf15a115505
Bruno Ruas <bruno.ruas2@gmail.com>
2023-05-31 20:30:02 -0400

    scrollView in login page

--- src/front/klug/pages/Login/index.js
+++ src/front/klug/pages/Login/index.js
@@ -1,7 +1,7 @@
 import styles from "./styles";
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { View, Image, Pressable, Alert } from 'react-native';
+import { View, Image, Pressable, Alert, ScrollView } from 'react-native';
 import { Text } from 'react-native-paper';
 import KlugInput from "../../components/Inputs/KlugInput";
 import KlugButton from "../../components/Buttons/KlugButtons";
@@ -75,43 +75,44 @@ export default function Login({ route, navigation }) {
   };
 
     return (
-    <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.title}>Login</Text>
-        </View>
+    <ScrollView>
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Login</Text>
+          </View>
 
-        <Image
-          source={{ uri: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e3-proj-mov-t3-pmv-ads-2023-1-e3-proj-mov-t3-klug/main/docs/img/logo-klug.png' }}
-          style={styles.Logo} />
+          <Image
+            source={{ uri: 'https://raw.githubusercontent.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e3-proj-mov-t3-pmv-ads-2023-1-e3-proj-mov-t3-klug/main/docs/img/logo-klug.png' }}
+            style={styles.Logo} />
 
-        <View style={styles.form}>
+          <View style={styles.form}>
 
-          <KlugInput label="Digite seu email"
-            handleInput={handleEmailChange}
-            security={false}
-          />
+            <KlugInput label="Digite seu email"
+              handleInput={handleEmailChange}
+              security={false}
+            />
 
-          <KlugInput label="Digite sua senha"
-            handleInput={handlePasswordChange}
-            security={true}
-          />
+            <KlugInput label="Digite sua senha"
+              handleInput={handlePasswordChange}
+              security={true}
+            />
 
-          {showError && <Text style={{ color: 'red', marginTop: 10, marginBottom: 2 }}>Usuario ou senha incorretos. Tente novamente.</Text>}
+            {showError && <Text style={{ color: 'red', marginTop: 10, marginBottom: 2 }}>Usuario ou senha incorretos. Tente novamente.</Text>}
 
-          <Pressable onPress={() => navigation.navigate('Recovery')}>
-            <Text style={styles.lostpsw}>Esqueci a Senha</Text>
+            <Pressable onPress={() => navigation.navigate('Recovery')}>
+              <Text style={styles.lostpsw}>Esqueci a Senha</Text>
+            </Pressable>
+          </View>
+            <KlugButton 
+              mode="contained" 
+              onPress={(handleLoginPress)} 
+              style={KlugButtonsStyles.button}
+              title='Entrar'>
+            </KlugButton>
+          <Pressable onPress={() => navigation.navigate('Register')}>
+              <Text style={styles.lostpsw}>Criar Conta</Text>
           </Pressable>
-        </View>
-          <KlugButton 
-            mode="contained" 
-            onPress={(handleLoginPress)} 
-            style={KlugButtonsStyles.button}
-            title='Entrar'>
-          </KlugButton>
-        <Pressable onPress={() => navigation.navigate('Register')}>
-            <Text style={styles.lostpsw}>Criar Conta</Text>
-        </Pressable>
-
-    </View>
+      </View>
+    </ScrollView>
   );
 }
